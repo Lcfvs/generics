5244d11f06d0f0161fe50e94411f2f0c3fc82fa6
Lcfvs <lcf.vs@hotmail.com>
2020-03-21 00:33:20 +0100

    Fix async reduce on field()

--- tests/index.js
+++ tests/index.js
@@ -2,38 +2,43 @@ import attempt from '@lcf.vs/generics/lib/express/attempt.js'
 import { query } from '@lcf.vs/generics/lib/express/parser.js'
 import logger from '@lcf.vs/generics/lib/log/logger.js'
 
-const rules = {
-  content: [
-    minString(5)
-  ]
-}
-
-const request = {
-  query: {
-    content: '12345'
-  }
-}
-
-const response = {}
-
-const next = error => console.log(error)
-
-const route = attempt([
-  query(rules),
-  logger()
-])
-
-
 function minString (min) {
   return async (value) => {
     const { length } = value
 
-    if (length >= min) {
+    if (length >= min) {console.log(min)
       return value
     }
 
     throw new Error(`Chaine trop courte (doit être supérieure ou égale à ${min} caractères)`)
   }
 }
+
+const response = {}
+
+const next = error => console.error(error)
+
+const rules = {
+  content: [
+    minString(5),
+    minString(5)
+  ],
+  content2: [
+    minString(5),
+    minString(5)
+  ]
+}
+
+const request = {
+  query: {
+    content: '12345',
+    content2: '12345'
+  }
+}
+
+const route = attempt([
+  query(rules),
+  logger()
+])
 
 void route(request, response, next)
