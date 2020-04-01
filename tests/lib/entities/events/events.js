1ac60c628e62d0dc607837a5ce1926c140098d72
Lcfvs <lcf.vs@hotmail.com>
2020-04-01 19:10:19 +0200

    rename w3c.string.line to w3c.string.text + .all() on each parsers, except misc

--- tests/lib/entities/events/events.js
+++ tests/lib/entities/events/events.js
@@ -11,19 +11,19 @@ export default {
     }
   },
   createdDate: {
-    min: '1970-01-01T00:00:00',
+    min: '1970-01-01T00:00',
     required: '',
     step: '1',
     type: 'datetime'
   },
   updatedDate: {
-    min: '1970-01-01T00:00:00',
+    min: '1970-01-01T00:00',
     step: '1',
     type: 'datetime',
     value: ''
   },
   archivedDate: {
-    min: '1970-01-01T00:00:00',
+    min: '1970-01-01T00:00',
     step: '1',
     type: 'datetime',
     value: null
@@ -35,14 +35,14 @@ export default {
     type: 'text'
   },
   startDate: {
-    min: '1970-01-01T00:00:00',
+    min: '1970-01-01T00:00',
     required: '',
     step: '1',
     type: 'datetime',
     value: null
   },
   endDate: {
-    min: '1970-01-01T00:00:00',
+    min: '1970-01-01T00:00',
     required: '',
     step: '1',
     type: 'datetime',
--- tests/lib/parsers/events/columns.js
+++ tests/lib/parsers/events/columns.js
@@ -12,35 +12,22 @@ const { events: {
 
 export default {
   content: [
-    parsers.w3c.string.type(content),
-    parsers.w3c.string.maxlength(content),
-    parsers.w3c.string.minlength(content),
-    parsers.w3c.string.line()
+    ...parsers.w3c.string.all(content)
   ],
   createdDate: [
-    parsers.w3c.datetime.type(createdDate),
-    parsers.w3c.datetime.min(createdDate),
-    parsers.w3c.datetime.step(createdDate)
+    ...parsers.w3c.datetime.all(createdDate)
   ],
   endDate: [
-    parsers.w3c.datetime.type(endDate),
-    parsers.w3c.datetime.min(endDate),
-    parsers.w3c.datetime.step(endDate)
+    ...parsers.w3c.datetime.all(endDate)
   ],
   id: [
-    parsers.w3c.number.type(id),
-    parsers.w3c.number.min(id),
-    parsers.w3c.number.step(id),
-    parsers.knex.type(id.context)
+    ...parsers.w3c.number.all(id),
+    ...parsers.knex.all(id.context)
   ],
   startDate: [
-    parsers.w3c.datetime.type(startDate),
-    parsers.w3c.datetime.min(startDate),
-    parsers.w3c.datetime.step(startDate)
+    ...parsers.w3c.datetime.all(startDate)
   ],
   updatedDate: [
-    parsers.w3c.datetime.type(updatedDate),
-    parsers.w3c.datetime.min(updatedDate),
-    parsers.w3c.datetime.step(updatedDate)
+    ...parsers.w3c.datetime.all(updatedDate)
   ]
 }
