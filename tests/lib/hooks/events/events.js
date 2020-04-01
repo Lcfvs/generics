4b702048c39ef024ff2b77876aae4cf4431b0347
Lcfvs <lcf.vs@hotmail.com>
2020-04-01 19:24:28 +0200

    Update events.js

--- tests/lib/hooks/events/events.js
+++ tests/lib/hooks/events/events.js
@@ -9,21 +9,26 @@ export default {
   archive: [
     ...hooks.request.input.all(events.archive),
     hooks.knex.archiveEntity(context)
+    // add any custom hooks here (like rendering hook)
   ],
   create: [
     ...hooks.request.input.all(events.create),
     hooks.knex.saveEntity(context)
+    // add any custom hooks here (like rendering hook)
   ],
   find: [
     ...hooks.request.input.all(events.find),
     hooks.knex.getEntity(context)
+    // add any custom hooks here (like rendering hook)
   ],
   search: [
     ...hooks.request.input.all(events.search),
     hooks.knex.searchEntities(context)
+    // add any custom hooks here (like rendering hook)
   ],
   update: [
     ...hooks.request.input.all(events.update),
     hooks.knex.saveEntity(context)
+    // add any custom hooks here (like rendering hook)
   ]
 }
